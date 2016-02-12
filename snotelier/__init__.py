d3efb907ba9795aae466ce1ea75914ed544645d9
Thomas Baldwin <tbaldwin@uoregon.edu>
2016-02-12 14:01:17 -0800

    logging

--- snotelier/__init__.py
+++ snotelier/__init__.py
@@ -3,6 +3,12 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
 
+if not app.debug:
+    import logging
+    handler = logging.FileHandler('snotelier.log')
+    handler.setLevel(logging.WARNING)
+    app.logger.addHandler(handler)
+
 app.config.from_pyfile('default.cfg')
 app.config.from_envvar('SNOTELIER_CONFIG', silent=True)
 
